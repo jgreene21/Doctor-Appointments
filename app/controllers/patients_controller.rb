feee33d3d4d8d376930e65514d470699ebcb993f
Julie Greene <julie_greene22@yahoo.com>
2020-02-24 17:00:53 -0700

    Doctors and Patients CRUD actions all complete

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,51 @@
+class PatientsController < ApplicationController
+  before_action :set_patient, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+
+    if @patient.save
+      redirect_to @patient
+    else
+      render :new
+    end
+  end
+
+  def edit 
+    render partial: "patients/form"
+  end 
+
+  def update
+    if @patient.update(patient_params)
+      redirect_to @patient
+      # same as redirect_to department_patient_path(@department, @patient)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @patient.destroy
+    redirect_to patients_path
+  end
+
+  private
+    def set_patient
+      @patient = Patient.find(params[:id])
+    end
+
+    def patient_params
+      params.require(:patient).permit(:first_name, :last_name)
+    end
+end
---
+++ app/models/patient.rb
@@ -0,0 +1,9 @@
+class Patient < ApplicationRecord
+  has_many :appointments 
+  has_many :doctors, through: :appointments, dependent: :destroy 
+
+def full_name 
+  "#{self.first_name} #{self.last_name}"
+end 
+
+end 
